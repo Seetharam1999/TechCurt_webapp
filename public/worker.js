23338b831df6ae99bcc682ee0efd857629d457f0
Seetharam1999 <srseetharam1999@gmail.com>
2020-09-14 23:18:14 +0530

    pwa

--- public/worker.js
+++ public/worker.js
@@ -1,48 +1,52 @@
-Let CACHE_NAME = 'your-app-name';
-Let urlsToCache = [
-  '/',
-  '/completed'
-];
+const staticAccess=[
+  './',
 
-// Install a service worker
-self.addEventListener('install', event => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+'./images/copyright.png',
+'./images/home.png',
+'./images/all.png',
+'./images/news.png',
+'./images/product.png',
+'./images/review.png',
+'./images/best.png',
+'./images/root.png',
+'./images/deals.png',
+'./images/password.png',
+'./images/share.png',
+'./images/refresh.png',
+'./images/search1.png'];
 
-// Cache and return requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
 
-// Update a service worker
-self.addEventListener('activate', event => {
-  Let cacheWhitelist = ['your-app-name'];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener("install",async e=>{
+  const cache=await caches.open('static');
+  cache.addAll(staticAccess)
+ 
 });
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  const url=new URL(req.url);
+  if(url.origin===location.origin){
+      e.respondWith(cacheFirst(req));
+  }  
+  else{
+      e.respondWith(networkFirst(req))
+  }
+  
+
+});
+async function cacheFirst(req){
+  const cachedResponse=await caches.match(req);
+  return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req){
+  const cache=await caches.open('static');
+  try{
+      const req=await fetch(req);
+      cache.pull(req,res.clone());
+      return res;
+  }
+
+  catch(err){
+      return await cache.match(req);
+  }
+}
